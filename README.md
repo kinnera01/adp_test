b2a797b3ffcf1f3f1df79c5f42149490e79b12b0
KinneraVinay <kinnera.bezawada1@gmail.com>
2019-01-09 22:07:05 -0500

    updating changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # adp_test
+
+## Install
+ To run this app you can clone this repo and in your root directory do 
+npm install 
+
--- server.js
+++ server.js
@@ -1,77 +1,93 @@
-//request package is used to make HTTP calls
+// assert = require('assert');
 let request = require("request");
 let colors = require('colors');
 //Global variables
 let _data;
 let _results;
 
+//getting data from URL
+function getAPI(){
 //to get data from URL
-request.get("https://interview.adpeai.com/api/v1/get-task", function (error, response, body) {
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
-        //get the response body and parsing it into Json obj ans store in _data variable
-        _data = JSON.parse(body)
-
-        console.log("Data Received :".yellow + '\n' + JSON.stringify(_data));
-
-        //based on data find out result
-        switch (_data.operation) {
-            case "addition":
-                _results = _data.left + _data.right;
-                break;
-            case "subtraction":
-                _results = _data.left - _data.right;
-                break;
-            case "multiplication":
-                _results = _data.left * _data.right;
-                break;
-            case "remainder":
-                _results = _data.left % _data.right;
-                break;
-            case "division":
-                _results = _data.left / _data.right;
-                break;
-            default:
-                _results = "That is an invalid operation!";
-                break;
-        }
-        console.log('---Getting ID and Results---'.red)
-        console.log("Id : ".yellow + _data.id + '\n' + "Result : ".yellow + _results);
-        //making an obj to do post request.
-
-        let options = {
-            uri: 'https://interview.adpeai.com/api/v1/submit-task',
-            method: 'POST',
-            body: {
-                "id": _data.id,
-                "result": _results
-            },
-            json: true,
-        };
-        request(options,
-            function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    console.log("Post Success !".green)
-                }
-                else if (!error && response.statusCode === 400) {
-                    console.log("Incorrect Value in result;no ID specified,value is invalid".red)
-                }
-                else if (!error && response.statusCode === 500) {
-                    console.log("ID cannot be found".red)
-                }else{
-                    console.log("API not found ".red + error)
-                }
+    request.get("https://interview.adpeai.com/api/v1/get-task", function (error, response, body) {
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+            //get the response body and parsing it into Json obj ans store in _data variable
+            _data = JSON.parse(body)
+    
+            console.log("Data Received :".yellow + '\n' + JSON.stringify(_data));
+    
+            //based on data find out result
+            switch (_data.operation) {
+                case "addition":
+                    _results = _data.left + _data.right;
+                    break;
+                case "subtraction":
+                    _results = _data.left - _data.right;
+                    break;
+                case "multiplication":
+                    _results = _data.left * _data.right;
+                    break;
+                case "remainder":
+                    _results = _data.left % _data.right;
+                    break;
+                case "division":
+                    _results = _data.left / _data.right;
+                    break;
+                default:
+                    _results = "That is an invalid operation!";
+                    break;
             }
-        );
-    }else{
-        if(response.statusCode === 404){
-            console.log(response.statusCode +' - API not found'.red);
-        }else if(response.statusCode === 400){
-            console.log(response.statusCode +' - Bad Request'.red);
+            console.log('---Getting ID and Results---'.red)
+            console.log("Id : ".yellow + _data.id + '\n' + "Result : ".yellow + _results);
+            //making an obj to do post request.
+            postAPI(_results);
         }else{
-            console.log(response.statusCode +' - Internal Server Error'.red);
+            if(response.statusCode === 404){
+                console.log(response.statusCode +' - API not found'.red);
+            }else if(response.statusCode === 400){
+                console.log(response.statusCode +' - Bad Request'.red);
+            }else{
+                console.log(response.statusCode +' - Internal Server Error'.red);
+            }
+            
         }
-        
-    }
-
+           
 })
+}
+
+//posting  data
+function postAPI(_resultsdata){
+    let options = {
+        uri: 'https://interview.adpeai.com/api/v1/submit-task',
+        method: 'POST',
+        body: {
+            "id": _data.id,
+            "result": _resultsdata
+        },
+        json: true,
+    };
+    request(options,
+        function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log("Post Success !".green)
+            }
+            else if (!error && response.statusCode === 400) {
+                console.log("Incorrect Value in result;no ID specified,value is invalid".red)
+            }
+            else if (!error && response.statusCode === 500) {
+                console.log("ID cannot be found".red)
+            }else{
+                console.log("API not found ".red + error)
+            }
+        }
+    );
+
+}
+
+//main function to run get&post request
+function main(){
+//setting intreval to run infinetly
+   setInterval(()=>{ getAPI()},1000)
+  
+}
+main();
